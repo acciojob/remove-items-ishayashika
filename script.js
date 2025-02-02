0808060bfe59b4f326315f6595fecc1cd0bead95
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-03 02:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function removeOption(){
+	let colorSelect=document.getElementById("colorSelect");
+	let selectOption=colorSelect.options[colorSelect.selectedIndex];
+	colorSelect.removeChild(selectOption);
+}
